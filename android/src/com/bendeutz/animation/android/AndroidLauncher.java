892438fc830914bf7b42cef8dbb21e29b5e40c60
bendeutz <benjamin.deutz@googlemail.com>
2015-03-27 19:00:13 +0100

    init

---
+++ android/src/com/bendeutz/animation/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.bendeutz.animation.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.bendeutz.animation.Animator;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Animator(), config);
+	}
+}
---
+++ desktop/src/com/bendeutz/animation/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.bendeutz.animation.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.bendeutz.animation.Animator;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Animator(), config);
+	}
+}
